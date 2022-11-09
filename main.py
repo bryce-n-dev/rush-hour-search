f8846c5ea9eb4c9b02f5cc32da757583e25e701c
Bryce Nichol <bryce.n.dev@icloud.com>
2022-11-08 21:13:12 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from queue import PriorityQueue # https://www.educative.io/answers/what-is-the-python-priority-queue
+
+q = PriorityQueue()
+
+
+class RushHour():
+    def __init__(self) -> None:
+        pass
+        # attributes of board
+
+    def move_right(self, vehicle):
+        pass
+
+    def move_left(self, vehicle):
+        pass
+
+    def move_down(self, vehicle):
+        pass
+
+    def move_up(self, vehicle):
+        pass
+
+    def is_solved(self):
+        pass
+
+
+# Parse values into 2D array
+
+# Print status
+
+# Move by increasing/decreasing index in one dimension
+# Subtract fuel by one, only move if enough fuel
+
+# Movement must be for all letters
+
+# Check for goal
