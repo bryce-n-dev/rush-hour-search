054c25d8712dea6c603db340816b60e3655822a6
Bryce Nichol <bryce.n.dev@icloud.com>
2022-11-30 01:16:07 -0500

    UCS algorithm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import utils
+from node import Node
+from ucs import uniform_cost_search
 
 
 if __name__ == "__main__":
@@ -6,63 +8,72 @@ if __name__ == "__main__":
     root_nodes = utils.init_boards("sample-input.txt") # List of initial games (represented as root nodes)
 
     # Get the root node of our first game
-    root_node = root_nodes[0]
+    for root_node in root_nodes:
+        final_node = uniform_cost_search(root_node)
 
-    # We can print the board
-    print("-----INITIAL BOARD STATE------")
-    root_node.state.print_board()
+        def print_node(node: Node):
+            if node is not None:
+                print_node(node.parent)
+                if node.move is not None:
+                    print(node.move)
 
-    # We can get any Node's children by calling get_children()
+        print_node(final_node)
 
-    # NOTE: For each child, the cost is incremented by 1.
-    # This will be useful for UCS, but I believe we can ignore it for other algorithms
-    children = root_node.get_children()
+    # # We can print the board
+    # print("-----INITIAL BOARD STATE------")
+    # root_node.state.print_board()
 
-    # children is a list of Node objects.
-    print("------CHILDREN------")
-    for child in children:
-        # Just as before, we can print the board state
-        print("-----CHILD------")
-        child.state.print_board() 
+    # # We can get any Node's children by calling get_children()
 
-        # We can print the move that was used to get to this state.
-        # This will be used when we need to print out the moves of the solution
-        print(f'Previous move: {child.move}') 
+    # # NOTE: For each child, the cost is incremented by 1.
+    # # This will be useful for UCS, but I believe we can ignore it for other algorithms
+    # children = root_node.get_children()
 
-        # We can also get the board state in the form of a string.
-        # Why is this important?
-        # We need to be able to store board states in a way that is easily searchable.
-        # Specifically, when we need to add states to the closed list.
-        # If any child's board state is already in the closet list, we do not need to explore it any further.
-        board_string = child.state.get_board_string()
-        print(f'Board string: {board_string}')
+    # # children is a list of Node objects.
+    # print("------CHILDREN------")
+    # for child in children:
+    #     # Just as before, we can print the board state
+    #     print("-----CHILD------")
+    #     child.state.print_board() 
 
-        # Another important part: we need to see if the board is in the winning state!
-        # This can be done as follows.
-        is_win = child.state.is_solved()
-        print(f'Did I win? {is_win}')
+    #     # We can print the move that was used to get to this state.
+    #     # This will be used when we need to print out the moves of the solution
+    #     print(f'Previous move: {child.move}') 
 
-        # Here's the fun part. We will need to use recursion here!
-        # I'm lazy so I will just call the function on each child to generate *its* children (the children of the children).
-        children_children = child.get_children()
-        print("---CHILDREN CHILDREN----")
+    #     # We can also get the board state in the form of a string.
+    #     # Why is this important?
+    #     # We need to be able to store board states in a way that is easily searchable.
+    #     # Specifically, when we need to add states to the closed list.
+    #     # If any child's board state is already in the closet list, we do not need to explore it any further.
+    #     board_string = child.state.get_board_string()
+    #     print(f'Board string: {board_string}')
 
-        # Same functions work! It's nodes all the way down.
-        for child in children_children:
-            child.state.print_board()
-            print(f'Previous move: {child.move}') 
-            board_string = child.state.get_board_string()
-            print(f'Board string: {board_string}')
-            is_win = child.state.is_solved()
-            print(f'Did I win? {is_win}')
-            children_children_children = child.get_children()
+    #     # Another important part: we need to see if the board is in the winning state!
+    #     # This can be done as follows.
+    #     is_win = child.state.is_solved()
+    #     print(f'Did I win? {is_win}')
 
-            # Add recursion here!
-            for child in children_children_children:
-                child.state.print_board()
-                print(f'Previous move: {child.move}') 
-                board_string = child.state.get_board_string()
-                print(f'Board string: {board_string}')
-                is_win = child.state.is_solved()
-                print(f'Did I win? {is_win}') # One of them actually finds the correct solution!!!
-                children_children_children = child.get_children()
+    #     # Here's the fun part. We will need to use recursion here!
+    #     # I'm lazy so I will just call the function on each child to generate *its* children (the children of the children).
+    #     children_children = child.get_children()
+    #     print("---CHILDREN CHILDREN----")
+
+    #     # Same functions work! It's nodes all the way down.
+    #     for child in children_children:
+    #         child.state.print_board()
+    #         print(f'Previous move: {child.move}') 
+    #         board_string = child.state.get_board_string()
+    #         print(f'Board string: {board_string}')
+    #         is_win = child.state.is_solved()
+    #         print(f'Did I win? {is_win}')
+    #         children_children_children = child.get_children()
+
+    #         # Add recursion here!
+    #         for child in children_children_children:
+    #             child.state.print_board()
+    #             print(f'Previous move: {child.move}') 
+    #             board_string = child.state.get_board_string()
+    #             print(f'Board string: {board_string}')
+    #             is_win = child.state.is_solved()
+    #             print(f'Did I win? {is_win}') # One of them actually finds the correct solution!!!
+    #             children_children_children = child.get_children()
--- node.py
+++ node.py
@@ -11,6 +11,24 @@ class Node():
         self.move = move
         self.cost = cost
 
+    def __eq__(self, other):
+        return (self.cost == other.cost)
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __lt__(self, other):
+        return (self.cost < other.cost)
+
+    def __gt__(self, other):
+        return (self.cost > other.cost)
+
+    def __le__(self, other):
+        return (self.cost < other.cost) or (self.cost == other.cost)
+
+    def __ge__(self, other):
+        return (self.cost > other.cost) or (self.cost == other.cost)
+
     def get_children(self) -> List[Node]:
         children = []
         cars = self.state.get_game_cars()
@@ -19,22 +37,22 @@ class Node():
                 if self.state.move_up_available(car, i):
                     child_state = self.state.move_up(car, i)
                     if child_state is not None:
-                        child_move = f'{car}{str(i)}up'
+                        child_move = f'{car} up {str(i)}'
                         children.append(Node(parent=self, state=child_state, move=child_move, cost=self.cost + 1))
                 if self.state.move_down_available(car, i):
                     child_state = self.state.move_down(car, i)
                     if child_state is not None:
-                        child_move = f'{car}{str(i)}down'
+                        child_move = f'{car} down {str(i)}'
                         children.append(Node(parent=self, state=child_state, move=child_move, cost=self.cost + 1))
                 if self.state.move_left_available(car, i):
                     child_state = self.state.move_left(car, i)
                     if child_state is not None:
-                        child_move = f'{car}{str(i)}left'
+                        child_move = f'{car} left {str(i)}'
                         children.append(Node(parent=self, state=child_state, move=child_move, cost=self.cost + 1))
                 if self.state.move_right_available(car, i):
                     child_state = self.state.move_right(car, i)
                     if child_state is not None:
-                        child_move = f'{car}{str(i)}right'
+                        child_move = f'{car} right {str(i)}'
                         children.append(Node(parent=self, state=child_state, move=child_move, cost=self.cost + 1))
         children = [i for i in children if i is not None] # Remove none values
         return children 
